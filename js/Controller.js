893e66b5a375b5133813f4e480546d3fbdcbd7d5
Jay Zhou <jayzho@amazon.com>
2021-03-23 23:16:44 -0400

    remove bin1 and update some photo

--- js/Controller.js
+++ js/Controller.js
@@ -1,4 +1,4 @@
-window.articleText = '作为一个爱旅行的码农，俺长着一颗漂泊的心和大腿，但我和同样喜欢旅行的她站在民政局的台子边，我想起我们还没有一起去过很多地方。所以俺写了这个网站，并且召集了世界各处的各路难兄难弟靠谱基友驴友狐朋狗友，在地球上的某个角落，举一块“余晓瑞嫁给周宁奕”的牌子。这其中，确实冒出了很多非常感动的片段，比如在委瑞内拉，数不相识Katrin Klingenberg同学拿着一块木牌子在各地拍了十几张照片，又比如在太行山大美女杨思同学在大家要启程回家赶路的时候还认真导演了惊艳的小剧本，又比如在尼泊尔珠穆朗玛峰南坡登山abc营地，登山家罗静在昨天还是大雪崩山难的地方前拍下照片，最后很兴奋的是，在一次开会的间隙，成功地拦截了大老板jack马，啊哈哈哈哈！！！.....感谢照片拍与被拍的:马云、飞鸟、杨思、驴小宝、毕大鹏、至善、彭家驹、黄涛、徐山乐、程东伟、宋梦想、安尘、登山家罗静、june、Katrin Klingenberg、格布、三刀、襄爷、鱼姐、刘博、黄如燕、彭心怡、候总、小欣、狒狒、王越、浩浩等'
+window.articleText = '三十多年前，有两个小生命诞生在这地球上，一个在陕西，一个在山东。2003年，她随着父母来到了加拿大，他还在中国的一个小镇读书。2013年，她研究生毕业了，他因缘巧合的申请到了加拿大的研究生。2015年，他研究生毕业，只身一人来到了多伦多。曾经有一段时间，他们住的很近，或许曾经擦肩而过，只是未曾相识。2019年8月，他们在两颗红豆上相识，在PAI相见，慢慢相知，相恋。2019年10月，他在加拿大最高的CN塔向她表白。2021年，他决定向她求婚，希望以后一生相守。他征求了全球各地朋友的祝福，希望和大家一起分享这份喜悦'
 window.titleText = '这个星球上的祝福 - the world wishes';
 ////////提示图层
 !(function(root) {
@@ -12,13 +12,6 @@ window.titleText = '这个星球上的祝福 - the world wishes';
     this.show();
   };
 
-  Controller.prototype.start = function() {
-    //场景开始后，面板开始有交互
-    this.hover();
-    this.hide();
-    this.startFunc();
-  }
-
   Controller.prototype.clicks = function() {
     var self = this;
     //开始按钮
@@ -30,6 +23,19 @@ window.titleText = '这个星球上的祝福 - the world wishes';
       }
     );
   }
+
+  Controller.prototype.start = function() {
+    //场景开始后，面板开始有交互
+    this.hover();
+    this.hide();
+    this.startFunc();
+  }
+  Controller.prototype.hide = function() {
+    $('#controller').css('top', '-82%').css('opacity', 0.3);
+    // $('#webgl').removeClass('blur');
+    // $('#divDisplay').removeClass('blur');
+  }
+
 
   Controller.prototype.hover = function() {
     var self = this;
@@ -55,13 +61,22 @@ window.titleText = '这个星球上的祝福 - the world wishes';
       }, (delay - 2000)
     );
 
-    setTimeout(function() {
-      if (!self.clickBol) {
-        self.hide;
-        self.start();
-      }
+    setTimeout(
+      function() {
+        if (!self.clickBol) {
+          self.hide;
+          self.start();
+        }
     }, delay);
   }
+
+
+  Controller.prototype.show = function() {
+    $('#controller').css('top', '0%').css('opacity', 0.7)
+    // $('#webgl').addClass('blur');
+    // $('#divDisplay').addClass('blur');
+  }
+
 
   Controller.prototype.text = function(text) {
     $('#article')
@@ -89,16 +104,7 @@ window.titleText = '这个星球上的祝福 - the world wishes';
   }
 
 
-  Controller.prototype.hide = function() {
-    $('#controller').css('top', '-82%').css('opacity', 0.3);
-    // $('#webgl').removeClass('blur');
-    // $('#divDisplay').removeClass('blur');
-  }
-  Controller.prototype.show = function() {
-    $('#controller').css('top', '0%').css('opacity', 0.7)
-    // $('#webgl').addClass('blur');
-    // $('#divDisplay').addClass('blur');
-  }
+
 
 
   Controller.prototype.bg = function(color) {
@@ -121,9 +127,9 @@ window.titleText = '这个星球上的祝福 - the world wishes';
     }
   }
 
-  function clearDiv(div) { ///////////////////
-    $(div).find('div').remove();
-  }
+  // function clearDiv(div) { ///////////////////
+  //   $(div).find('div').remove();
+  // }
 
   root.Controller = Controller;
 })(window);
